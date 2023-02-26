a1a556f608df08c87ec4096cf9923f56057a6016
HelenFrosty <yelena_morozova_02@internet.ru>
2023-02-26 22:17:37 +0300

    Commit1

---
+++ src/Node.java
@@ -0,0 +1,21 @@
+public class Node {
+    private int num;
+    private Node next;
+    
+    public void setNum(int _num){
+       num = _num;
+    }
+    
+    public int getNum(){
+        return num;
+    }
+    
+    public void setNext(Node _next) {
+        next = _next;
+    }
+    
+    public Node getNext() {
+        return next;
+    }
+    
+}
